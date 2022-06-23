25dc08b840e473e00cf77611634a2a16d87e8422
Tamim Iqbal <108043975+IamTIqbal@users.noreply.github.com>
2022-06-23 21:31:29 +0600

    main

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=100%, initial-scale=1.0">
-    <link href="/css/style.css" rel="stylesheet">
+    <link href="/css/style.css" rel="stylesheet" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <title>Query form Database</title>
 </head>
 <body>
@@ -13,7 +13,7 @@
             <div class="col-md-12">
                 <div class="card mt-4">
                     <div class="card-header">
-                        <h4>Certificate E-Verify | Akbor Ali Institute of Information Technology </h4>
+                        <h4>Data Query | MySql Database </h4>
                     </div>
                     <div class="card-body">
                         <div class="row">
@@ -21,7 +21,9 @@
 
                                 <form action="" method="GET">
                                     <div class="input-group mb-3">
-                                        <input type="number" name="search" min="20000000" max="39999999" oninvalid="this.setCustomValidity('Enter ID Number Here')" required value="<?php if(isset($_GET['search'])){echo $_GET['search']; } ?>" class="form-control" placeholder="Type Your ID No">
+                                        <input type="number" name="search" oninvalid="this.setCustomValidity('Enter customer ID Here')" required 
+                                        value="<?php if(isset($_GET['search'])){echo $_GET['search']; } ?>"
+                                        class="form-control" placeholder="Type Anythings">
                                         <button type="submit" class="btn btn-primary">Search</button>
                                     </div>
                                 </form>
@@ -38,12 +40,12 @@
                         <table class="table table-striped table-bordered" width="100%">
                             <tbody>
                              <?php 
-                                    $con = mysqli_connect("sql102.vibd.eu.org","vibd_31680043","IL7H@SFQ","vibd_31680043_everify");
+                                    $con = mysqli_connect("sql.localhost","username","password","database name");
 
                                     if(isset($_GET['search']))
                                     {
                                         $filtervalues = $_GET['search'];
-                                        $query = "SELECT * FROM certificate WHERE CONCAT(uid,id,name,fname,dob,course,download) LIKE '%$filtervalues%' ";
+                                        $query = "SELECT * FROM table_name WHERE CONCAT(customer_id ) LIKE '%$filtervalues%' ";
                                         $query_run = mysqli_query($con, $query);
 
                                         if(mysqli_num_rows($query_run) > 0)
@@ -52,47 +54,40 @@
                                             {
                                                 ?>
                             <tr>
-                                <td>ID No</td>
-                                <td><?= $items['id']; ?></td>
-                                <td>Certificate Key</td>
-                                <td><?= $items['uid']; ?></td>
+                                <td>Customer Id</td>
+                                <td><?= $items['customer_id']; ?></td>
+                                <td>Customer Type</td>
+                                <td><?= $items['ctype']; ?></td>
                             </tr>
                             <tr>
-                                <td>Student Name</td>
+                                <td>Customer Name</td>
                                 <td colspan="3"><?= $items['name']; ?></td>
                             </tr>
                             <tr>
-                                <td>Father's Name</td>
-                                <td colspan="3"><?= $items['fname']; ?></td>
+                                <td>Customer's Company Name</td>
+                                <td colspan="3"><?= $items['company']; ?></td>
                             </tr>
                             <tr>
-                                <td>Session</td>
-                                <td><?= $items['syear']; ?></td>
-                                <td>Branch Code</td><td><?= $items['brnc']; ?></td>
+                                <td>Member Valid</td>
+                                <td><?= $items['date']; ?></td>
+                                <td>Branch Code</td><td><?= $items['branch_name']; ?></td>
                             </tr>
                             <tr>
                                 <td>Gender</td>
-                                <td><?= $items['ssex']; ?></td>
+                                <td><?= $items['csex']; ?></td>
                                 <td>Batch No</td>
-                                <td><?= $items['brnc']; ?></td>
+                                <td><?= $items['branch_name']; ?></td>
                             </tr>
                             <tr>
-                                <td>Result</td>
-                                <td><?= $items['cgpa']; ?></td>
-                                <td>Date of Birth</td>
-                                <td><?= $items['dob']; ?></td>
+                                <td>Colum</td>
+                                <td><?= $items['ddb']; ?></td>
+                                <td>Colum</td>
+                                <td><?= $items['ddb']; ?></td>
                             </tr>
                             <tr>
-                                <td>Course Name</td>
+                                <td>Colum</td>
                                 <td colspan="3">
                                 <span id="i_name"><?= $items['course']; ?></span>
-                                </td>
-                            </tr>
-                             <tr>
-                                <td colspan="4">
-                                <a href="<?= $items['download']; ?>" class="btn btn-info"><span class="glyphicon glyphicon-arrow-down"></span> Download</a>
-                                <a onclick="window.print()" class="btn btn-success"><span class="glyphicon glyphicon-print"></span> Print</a>
-                                <a onclick="history.back()" class="btn btn-success"><span class="glyphicon glyphicon-step-backward"></span> Back</a>
                                 </td>
                             </tr>
                             </tbody>  
@@ -117,6 +112,8 @@
             </div>
         </div>
     </div>
-    <script href="js/main.js" > </script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="js/main.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"> </script>
 </body>
 </html>
